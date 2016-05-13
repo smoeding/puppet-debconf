044ec1b37fa8b5c28fbef0b5d67b413ebc7d3ae9
Stefan Möding <stm@kill-9.net>
2016-05-13 17:56:37 +0200

    Update type descripton

--- lib/puppet/type/debconf.rb
+++ lib/puppet/type/debconf.rb
@@ -1,4 +1,4 @@
-# debconf.rb --- Type for the debconf provider
+# debconf.rb --- The debconf type
 
 Puppet::Type.newtype(:debconf) do
   desc <<-EOT
@@ -7,6 +7,11 @@ Puppet::Type.newtype(:debconf) do
     uses multiple programs from the 'debconf' package.
 
     Examples:
+
+        debconf { 'tzdata/Areas':
+          type  => 'select',
+          value => 'Europe',
+        }
 
         debconf { 'dash/sh':
           type  => 'boolean',
@@ -26,30 +31,32 @@ Puppet::Type.newtype(:debconf) do
   end
 
   newparam(:item, :namevar => true) do
-    desc %{The item name. This string must have the following format:
-      the package name, a literal slash char and the name of the question.}
+    desc %{The item name. This string must have the following format: the
+      package name, a literal slash char and the name of the question (e.g.
+      'tzdata/Areas').}
 
     newvalues(/^[a-z0-9][a-z0-9:.+-]+\/[a-zA-Z0-9\/_.+-]+$/)
   end
 
   newparam(:package) do
-    desc %{The package the item belongs to. The default is the first part
-      (up to the first '/') of the item parameter.}
+    desc %{The package the item belongs to. The default is the first part (up
+      to the first '/') of the item parameter (e.g. 'tzdata').}
 
     newvalues(/^[a-z0-9][a-z0-9:.+-]+$/)
     defaultto { @resource[:item].split('/', 2).first }
   end
 
   newparam(:type) do
-    desc %{The type of the item. This can be one of the following values:
-      string, boolean, select, multiselect, note, text, password, title}
+    desc %{The type of the item. This can only be one of the following
+      values: string, boolean, select, multiselect, note, text, password,
+      title.}
 
     newvalues(:string, :boolean, :select, :multiselect,
               :note, :text, :password, :title)
   end
 
   newproperty(:value) do
-    desc %{The value for the item.}
+    desc %{The value for the item (e.g. 'Europe').}
 
     newvalues(/\S/)
     munge { |value| value.strip } # Remove leading and trailing spaces
