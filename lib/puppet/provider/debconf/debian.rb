d1cfade4e163e3aa90c78ad4e45dfda756c596ff
Stefan Möding <stm@kill-9.net>
2016-05-13 17:06:55 +0200

    Update provider implementation

--- lib/puppet/provider/debconf/debian.rb
+++ lib/puppet/provider/debconf/debian.rb
@@ -1,9 +1,9 @@
 # debian.rb --- Debian provider for debconf type
+
 Puppet::Type.type(:debconf).provide(:debian) do
-  desc "Debian debconf management."
+  desc %q{Manage debconf database entries on Debian based systems.}
 
   confine :osfamily => :debian
-
   defaultfor :osfamily => :debian
 
   commands :debconf_show => "/usr/bin/debconf-show"
@@ -13,27 +13,30 @@ Puppet::Type.type(:debconf).provide(:debian) do
     @properties = Hash.new
   end
 
-  # Return the regular expression used to parse the debconf-show output
-  def regexp
-    Regexp.new(
-      "^(.) " +                       # Seen marker
-      "(?<pkg>[a-zA-Z0-9_.+-]+)" +    # Package name
-      "\/" +                          # Literal '/'
-      "(?<key>[a-zA-Z0-0\/_.+-]+):" + # Item name
-      "\s+" +                         # Space
-      "(?<val>.*?)\s*$")              # Value
-  end
+  # The regular expression used to parse the debconf-show output
+  DEBCONF_REGEXP = Regexp.new(
+    "^(.) " +                 # seen marker
+    "([a-z0-9.+-]+)" +        # package name
+    "\/" +                    # literal '/'
+    "([a-zA-Z0-9\/_.+-]+)" +  # item name
+    ":\s*" +                  # literal ':' and space
+    "(.*)?" +                 # value (optional)
+    "\s*$"                    # optional trailing spaces
+  )
 
   # Fetch all items and their values for a package
   def fetch
-    Puppet.debug("Debconf: caching data for #{resource[:item]}")
+    Puppet.debug("Debconf: caching data for #{resource[:name]}")
     Puppet.debug("Debconf: getting items for package #{resource[:package]}")
 
     debconf_show(resource[:package]).split("\n").each do |line|
-      m = regexp.match(line)
-      if m
-        Puppet.debug("Debconf: item #{m[:pkg]}/#{m[:key]} => #{m[:val]}")
-        @properties["#{m[:pkg]}/#{m[:key]}"] = m[:val]
+      if DEBCONF_REGEXP.match(line)
+        pkg, key, val = $2, $3, $4
+
+        Puppet.debug("Debconf: item #{pkg}/#{key} => #{val}")
+        @properties["#{pkg}/#{key}"] = val
+      else
+        Puppet.warning("Debconf: entry not parsed (#{line})")
       end
     end
   end
@@ -48,12 +51,14 @@ Puppet::Type.type(:debconf).provide(:debian) do
     IO.popen('/usr/bin/debconf-set-selections', 'w+') do |pipe|
       pipe.puts(args)
       pipe.close_write
+
+      # Ignore all we can read
       pipe.read.split("\n").each { |l| }
     end
   end
 
   def create
-    Puppet.debug("Debconf: create #{resource[:name]}")
+    Puppet.debug("Debconf: calling create #{resource[:name]}")
     fetch if @properties.empty?
 
     update
@@ -64,8 +69,10 @@ Puppet::Type.type(:debconf).provide(:debian) do
     fetch if @properties.empty?
 
     IO.popen("/usr/bin/debconf-communicate #{resource[:package]}", 'w+') do |pipe|
-      pipe.puts("UNREGISTER #{resource[:item]}")
+      pipe.puts("UNREGISTER #{resource[:package]}/#{resource[:item]}")
       pipe.close_write
+
+      # Parse return code
       pipe.read.split("\n").each do |l|
         fail("Debconf: debconf-communicate failed (#{l})") unless l =~ /^0+$/
       end
@@ -79,14 +86,14 @@ Puppet::Type.type(:debconf).provide(:debian) do
   end
 
   def value
-    Puppet.debug("Debconf: getting #{resource[:item]}")
+    Puppet.debug("Debconf: get #{resource[:item]}")
     fetch if @properties.empty?
 
     @properties[resource[:item]]
   end
 
   def value=(val)
-    Puppet.debug("Debconf: setting #{resource[:item]} to #{val}")
+    Puppet.debug("Debconf: set #{resource[:item]} to #{val}")
     fetch if @properties.empty?
 
     @properties[resource[:item]] = val
--- lib/puppet/type/debconf.rb
+++ lib/puppet/type/debconf.rb
@@ -1,34 +1,57 @@
-# exec { 'debconf_dash':
-#   command => 'echo "dash dash/sh boolean true"|debconf-set-selections',
-#   unless  => 'debconf-show dash|grep -q "dash/sh: *true"',
-#   require => Package['dash'],
-# }
-#
+# debconf.rb --- Type for the debconf provider
+
 Puppet::Type.newtype(:debconf) do
+  desc <<-EOT
+    Manage debconf database entries on Debian based systems. This type
+    can either set or remove a value for a debconf database entry. It
+    uses multiple programs from the 'debconf' package.
 
-  desc %q{Manage debconf database entries on Debian based systems.}
+    Examples:
 
-  ensurable
+        debconf { 'dash/sh':
+          type  => 'boolean',
+          value => 'true',
+        }
+
+        debconf { 'libraries/restart-without-asking':
+          package => 'libc6',
+          type    => 'boolean',
+          value   => 'true',
+        }
+  EOT
+
+  ensurable do
+    defaultvalues
+    defaultto :present
+  end
 
   newparam(:item, :namevar => true) do
-    desc %{The item to manage.}
-    newvalues(/^[a-zA-Z0-9_.+-]+\/[a-zA-Z0-9\/_.+-]+$/)
+    desc %{The item name. This string must have the following format:
+      the package name, a literal slash char and the name of the question.}
+
+    newvalues(/^[a-z0-9][a-z0-9:.+-]+\/[a-zA-Z0-9\/_.+-]+$/)
   end
 
   newparam(:package) do
-    desc %{The package the item belongs to.}
+    desc %{The package the item belongs to. The default is the first part
+      (up to the first '/') of the item parameter.}
 
-    newvalues(/^[a-zA-Z0-9_.+-]+$/)
+    newvalues(/^[a-z0-9][a-z0-9:.+-]+$/)
     defaultto { @resource[:item].split('/', 2).first }
   end
 
   newparam(:type) do
-    desc %{The type of the item.}
+    desc %{The type of the item. This can be one of the following values:
+      string, boolean, select, multiselect, note, text, password, title}
+
     newvalues(:string, :boolean, :select, :multiselect,
               :note, :text, :password, :title)
   end
 
   newproperty(:value) do
     desc %{The value for the item.}
+
+    newvalues(/\S/)
+    munge { |value| value.strip } # Remove leading and trailing spaces
   end
 end
